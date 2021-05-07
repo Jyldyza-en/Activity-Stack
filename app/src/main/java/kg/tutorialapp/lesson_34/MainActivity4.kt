af49bd22c84a16b78a49c957f779a18486484dbc
Jyldyz Sultanbekova <sultanbekova.jyldyz@gmail.com>
2021-05-07 16:19:19 +0600

    consider examples with singleTop,singleTask, singleInstance and Standard

--- app/src/main/java/kg/tutorialapp/lesson_34/MainActivity4.kt
+++ app/src/main/java/kg/tutorialapp/lesson_34/MainActivity4.kt
@@ -1,11 +1,34 @@
 package kg.tutorialapp.lesson_34
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+
+        val startDD = findViewById<Button>(R.id.button4)
+        startDD.setOnClickListener{ start(this)}
+
+        val startC = findViewById<Button>(R.id.button5)
+        startC.setOnClickListener{ start(this)}
+
+        val startA = findViewById<Button>(R.id.button6)
+        startA.setOnClickListener{ start(this)}
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+    }
+
+    companion object{
+        fun start(context: Context){
+            val intent = Intent(context, MainActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }
